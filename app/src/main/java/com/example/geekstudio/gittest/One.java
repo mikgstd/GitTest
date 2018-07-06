2d33ee0a4da7d4c138543ad99c5d44496d40aff3
Mikhail Ivanov <mikgstd@gmail.com>
2018-07-06 09:56:29 +0400

    Three commit

--- app/src/main/java/com/example/geekstudio/gittest/One.java
+++ app/src/main/java/com/example/geekstudio/gittest/One.java
@@ -4,5 +4,7 @@ public class One {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("lol");
+        System.out.println("I");
+        System.out.println("O");
     }
 }

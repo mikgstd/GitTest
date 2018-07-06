0543f7e6c9135b9eebf4cc233401f22cfd8dee61
Mikhail Ivanov <mikgstd@gmail.com>
2018-07-06 09:54:17 +0400

    Two commit

---
+++ app/src/main/java/com/example/geekstudio/gittest/One.java
@@ -0,0 +1,8 @@
+package com.example.geekstudio.gittest;
+
+public class One {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("lol");
+    }
+}
